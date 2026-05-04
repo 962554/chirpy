a3010b9bf562c4c9e52b1e92b361f74eda3b8548
962554 <254557023+962554@users.noreply.github.com>
2026-05-04 12:30:49 +0100

    feat: add readiness endpoint; update fileserver endpoint

--- main.go
+++ main.go
@@ -20,6 +20,13 @@ func main() {
 		ReadHeaderTimeout: readTimeout,
 	}
 
+	mux.Handle("/app/", http.StripPrefix("/app/", http.FileServer(http.Dir("."))))
+	mux.HandleFunc("/healthz", func(w http.ResponseWriter, r *http.Request) {
+		header := w.Header()
+		header.Add("Content-Type", "text/plain; charset=utf-8")
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte(http.StatusText(http.StatusOK)))
+	})
 	log.Printf("http server starting on port: %s", port)
 	log.Fatal(server.ListenAndServe())
 }
